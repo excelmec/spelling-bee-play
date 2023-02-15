95675bccf850079f5176d08c8fa857fb63220711
Allen Shibu <93600615+alapanoski@users.noreply.github.com>
2023-02-15 21:18:14 +0530

    Add files to use as a template

---
+++ to_be_merged/pages/api/bee.tsx
@@ -0,0 +1,16 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  fetch("https://www.nytimes.com/puzzles/spelling-bee")
+    .then((res) => res.text())
+    .then((data) => {
+      // console.log(data);
+      return JSON.parse(
+        data.slice(
+          data.indexOf("gameData") + 11,
+          data.indexOf("}}", data.indexOf("gameData")) + 2
+        )
+      );
+    })
+    .then((data) => res.status(200).json({ gameData: data }));
+}
