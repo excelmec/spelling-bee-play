4442275dc346726eceaa94b59f4e30c1e7c2ea8a
jaison080 <jaisondennis080@gmail.com>
2023-02-16 11:37:44 +0530

    Sytling somewhat completed

--- src/components/Game/buttons.tsx
+++ src/components/Game/buttons.tsx
@@ -1,13 +1,37 @@
-import {FiRefreshCcw} from 'react-icons/fi'
+import { FiRefreshCcw } from "react-icons/fi";
 
-export default function Buttons({ searchWord, shuffle, clearWord, revealedAnswers }) {
+export default function Buttons({
+  searchWord,
+  shuffle,
+  clearWord,
+  revealedAnswers,
+}) {
   return (
-    <div className="buttons">
-      <button onClick={() => clearWord()} className="single-btn">Delete</button>
-      <button className="shuffle-btn" data-testid='shuffle-btn' onClick={() => shuffle()} disabled={revealedAnswers}>
+    <div className="buttons" style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      gap: "1rem",
+      marginLeft: "2rem",
+    }}>
+      <button onClick={() => clearWord()} className="delete-btn">
+        Delete
+      </button>
+      <button
+        className="refresh-btn"
+        data-testid="shuffle-btn"
+        onClick={() => shuffle()}
+        disabled={revealedAnswers}
+      >
         <FiRefreshCcw />
       </button>
-      <button onClick={() => searchWord()} className="single-btn" disabled={revealedAnswers}>Enter</button>
+      <button
+        onClick={() => searchWord()}
+        className="enter-btn"
+        disabled={revealedAnswers}
+      >
+        Enter
+      </button>
     </div>
-  )
+  );
 }
