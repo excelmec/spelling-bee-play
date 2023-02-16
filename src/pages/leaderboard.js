0d97d2bc9de8d1afdfd664e5c6748b10f6d81278
jaison080 <jaisondennis080@gmail.com>
2023-02-16 14:28:48 +0530

    Title Issue Fixed

--- src/pages/leaderboard.js
+++ src/pages/leaderboard.js
@@ -1,10 +1,11 @@
 import Leaderboard from "../components/Leaderboard/Leaderboard";
-import { Navbar, Footer } from "../components";
+import CustomTitle from "../utils/customTitle";
 import MainLayout from "../components/MainLayout/MainLayout";
 
 export default function leaderboard() {
   return (
     <MainLayout>
+      <CustomTitle title="Leaderboard" />
       <div
         className="leaderboard"
         style={{
