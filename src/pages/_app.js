7465a0ba0d2d1feeecd78e12dc9ff78c6797ff8c
jaison080 <jaisondennis080@gmail.com>
2023-02-17 22:11:24 +0530

    Integrated Google Analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,22 +7,18 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Script
-        strategy="afterInteractive"
         src="https://www.googletagmanager.com/gtag/js?id=G-TERD0ELCVE"
-      />
-      <Script
-        id="google-analytics"
         strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'G-TERD0ELCVE', {
-page_path: window.location.pathname,
-});`,
-        }}
       />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-TERD0ELCVE');
+        `}
+      </Script>
       <UserDetails>
         <Toaster
           style={{
