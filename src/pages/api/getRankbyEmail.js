cb0acdfeb252b8a30e81f3d348197f8b3f5e804c
jaison080 <jaisondennis080@gmail.com>
2023-02-18 16:57:02 +0530

    Cleared Data for Launch

--- src/pages/api/getRankbyEmail.js
+++ src/pages/api/getRankbyEmail.js
@@ -1,7 +1,14 @@
+import NextCors from "nextjs-cors";
 import userModel from "../../models/userModel";
 import connectDB from "../../utils/connectDB";
 
 export default async function handler(req, res) {
+  await NextCors(req, res, {
+    // Options
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+    origin: "*",
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  });
   if (req.method === "POST") {
     await connectDB();
     const { email } = req.body;
