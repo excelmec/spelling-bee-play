2d5248985ac2ab4ba080e7d925bbc8090dbda1d5
jaison080 <jaisondennis080@gmail.com>
2023-02-18 10:59:56 +0530

    Updated Footer

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,1 +1,3 @@
-export {default as excelLogo} from './excellogo.png'
+export { default as excelLogo } from "./excellogo.png";
+export { default as mecLogo } from "./meclogo.png";
+export { default as excelMainLogo } from "./excelmainlogo.svg";
