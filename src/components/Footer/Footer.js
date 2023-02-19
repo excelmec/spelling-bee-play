55b5620b61ae987e67a99ced89f79c2f6f919926
jaison080 <jaisondennis080@gmail.com>
2023-02-19 10:06:56 +0530

    Removed Old Answers

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -14,7 +14,7 @@ function Footer() {
   return (
     <>
       <div className={styles.footer_container}>
-        <a href="https://excelmec.org/">
+        <a href="https://excelmec.org/" target="_blank" rel="noreferrer">
           <Image
             src={excelmainlogo}
             alt="Excel Logo"
@@ -24,19 +24,39 @@ function Footer() {
 
         <div className={styles.footer_text}>Made with ❤️ Excel 2022</div>
         <div className={styles.footer_icons}>
-          <a href="https://www.facebook.com/excelmec" target="_blank">
+          <a
+            href="https://www.facebook.com/excelmec"
+            target="_blank"
+            rel="noreferrer"
+          >
             <FaFacebookF className={styles.footer_icon} />
           </a>
-          <a href="https://twitter.com/excelmec">
+          <a
+            href="https://twitter.com/excelmec"
+            target="_blank"
+            rel="noreferrer"
+          >
             <FaTwitter className={styles.footer_icon} />
           </a>
-          <a href="https://www.instagram.com/excelmec/">
+          <a
+            href="https://www.instagram.com/excelmec/"
+            target="_blank"
+            rel="noreferrer"
+          >
             <FaInstagram className={styles.footer_icon} />
           </a>
-          <a href="https://www.linkedin.com/company/excelmec/">
+          <a
+            href="https://www.linkedin.com/company/excelmec/"
+            target="_blank"
+            rel="noreferrer"
+          >
             <FaLinkedinIn className={styles.footer_icon} />
           </a>
-          <a href="https://www.youtube.com/excelmec">
+          <a
+            href="https://www.youtube.com/excelmec"
+            target="_blank"
+            rel="noreferrer"
+          >
             <FaYoutube className={styles.footer_icon} />
           </a>
         </div>
